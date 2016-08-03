696e0d43b63618a561ad5e8c6a696f07972f8ac0
Megan King <mgnking12@gmail.com>
2016-08-03 16:50:03 -0500

    file name changes

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
 var nav = [{
-    Link: '/Dogs',
-    Text: 'Dogs'
+    Link: '/Pets',
+    Text: 'Pets'
 }, {
-    Link: '/Cats',
-    Text: 'Cats'
+    Link: '/Profile',
+    Text: 'Profile'
 }];
-var bookRouter = require('./src/routes/bookRoutes.js')(nav);
+// var profileRouter = require('./src/routes/profileRoutes.js')(nav);
+var petRouter = require('./src/routes/petRoutes.js')(nav);
 var adminRouter = require('./src/routes/adminRoutes.js')(nav);
 app.use(express.static('public'));
 // app.use(express.static('src/views'));
@@ -22,46 +23,19 @@ app.set('views', './src/views');
 
 // change .hbs to jade, or ejs if you'd rather use jade or ejs and vice versa
 app.set('view engine', 'ejs');
-var dogs = [];
-bookRouter.route('/Dogs')
-    .get(function(req, res) {
-        res.render('dogs', {
-            title: 'Dogs',
-            nav: [{
-                Link: '/Dogs',
-                Text: 'Dogs'
-            }, {
-                Link: '/Cats',
-                Text: 'Cats'
-            }]
-        });
-    });
-app.use('/', bookRouter);
-bookRouter.route('/Cats')
-    .get(function(req, res) {
-        res.render('cats', {
-            title: 'Cats',
-            nav: [{
-                Link: '/Dogs',
-                Text: 'Dogs'
-            }, {
-                Link: '/Cats',
-                Text: 'Cats'
-            }]
-        });
-    });
-bookRouter.route('/single')
-    .get(function(req, res) {
-        res.send('hello single dogs from bookRouter');
-    });
+// app.use('/Profile', profileRouter);
+app.use('/Pets', petRouter);
+app.use('/Admin', adminRouter);
 
 app.get('/', function(req, res) {
     res.render('index', {
-        title: 'hello from render',
+        title: 'Adopt!',
+        nav: nav
     });
 });
-// app.get('/dogs', function(req, res) {
-//     res.send('hello dogs');
+
+// app.get('/Pets', function(req, res) {
+//     res.send('hello Pets');
 // });
 app.listen(port, function(err) {
     console.log('running on port ' + port);
