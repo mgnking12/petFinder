c98ca4136272e192b4a2b92535ad2ddb280e0adc
Megan King <mgnking12@gmail.com>
2016-08-08 12:52:30 -0500

    card swipe

--- src/routes/petRoutes.js
+++ src/routes/petRoutes.js
@@ -4,19 +4,27 @@ var router = function(nav) {
     var pets = [{
         'name': 'Organa',
         'age': '1',
-        'type': 'Dog'
+        'type': 'Dog',
+        'img': 'http://placehold.it/350x150',
+        'description': 'Bacon ipsum dolor amet filet mignon turducken sirloin pork prosciutto, porchetta frankfurter pork belly pig ball tip drumstick.'
     }, {
         'name': 'Kevin',
         'age': '5',
-        'type': 'Cat'
+        'type': 'Cat',
+        'img': 'http://placehold.it/350x150',
+        'description': 'Bacon ipsum dolor amet filet mignon turducken sirloin pork prosciutto, porchetta frankfurter pork belly pig ball tip drumstick.'
     }, {
         'name': 'Max',
         'age': '14',
-        'type': 'Dog'
+        'type': 'Dog',
+        'img': 'http://placehold.it/350x150',
+        'description': 'Bacon ipsum dolor amet filet mignon turducken sirloin pork prosciutto, porchetta frankfurter pork belly pig ball tip drumstick.'
     }, {
         'name': 'Otto',
         'age': '2',
-        'type': 'Dog'
+        'type': 'Dog',
+        'img': 'http://placehold.it/350x150',
+        'description': 'Bacon ipsum dolor amet filet mignon turducken sirloin pork prosciutto, porchetta frankfurter pork belly pig ball tip drumstick.'
     }];
     petRouter.route('/')
         .get(function(req, res) {
